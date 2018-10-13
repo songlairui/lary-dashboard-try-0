7669b588e0835b09a426ef5158da069359fcba4c
lary <songlairui@qq.com>
2018-10-13 14:38:14 +0800

    添加 TODO 示例

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    )
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      text: ''
+    }
+    ;['handleChange', 'handleSubmit'].forEach(m => {
+      this[m] = this[m].bind(this)
+    })
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    if (!this.state.text.length) {
+      return
+    }
+    const newItem = {
+      text: this.state.text,
+      id: +new Date()
+    }
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }))
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -20,9 +58,24 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <main>
+          <div className="lary-todo">
+            <h3>TODO</h3>
+            <TodoList items={this.state.items} />
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="new-todo">next to be done</label>
+              <input
+                id="new-todo"
+                onChange={this.handleChange}
+                value={this.state.text}
+              />
+              <button>Add #{this.state.items.length + 1}</button>
+            </form>
+          </div>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
